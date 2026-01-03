ef469ad615c086b5dbb6034b2c05a159e8734f50
Meherab Hossen <mhnnishat0102@gmail.com>
2026-01-04 03:37:55 +0600

    login completed

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -24,7 +24,69 @@ class login extends StatelessWidget {
             ),),
             SizedBox(height: 10),
 
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: TextField(
+                decoration: InputDecoration(
 
+                  hintText: 'Username',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10)
+                  )
+                ),
+              ),
+            ),
+            //SizedBox(height: 1,),
+
+            Padding(padding: const EdgeInsets.all(15),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                ),
+              ),
+            ),
+            Padding(padding: const EdgeInsets.all(15),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromRGBO(54, 109, 231, 1.0),
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadiusGeometry.circular(10)
+                      ),
+                    ),
+                    onPressed: (){
+                  print('Login button pressed');
+                }, child: Text('Login')),
+              )
+            ),
+
+            SizedBox(
+              child: TextButton(onPressed: (){
+                print('Forget password button pressed');
+              },
+                  child: Text('Forgot Password?',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.black.withOpacity(0.5)
+                ),
+              )),
+            ),
+            SizedBox(
+              child: TextButton(onPressed: (){
+                print('Sign up button pressed');
+              },
+                  child: Text('Don\'t have an account? Sign up',
+                style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.black.withOpacity(0.5)
+                ),
+              )),
+            ),
           ],
         ),
       )
